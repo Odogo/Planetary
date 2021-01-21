b65d6101bd40e751d826f7a8ad46480c115e6de1
Odogo <77793565+Odogo@users.noreply.github.com>
2021-01-21 11:14:41 -0600

    Configuration Setup
    Added the default configuration file and default attributes file.

---
+++ src/odogo/plantary/Planetary.java
@@ -0,0 +1,19 @@
+package odogo.plantary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Planetary extends JavaPlugin {
+
+	@Override public void onLoad() {
+
+	}
+
+	@Override public void onEnable() {
+
+	}
+
+	@Override public void onDisable() {
+
+	}
+
+}
